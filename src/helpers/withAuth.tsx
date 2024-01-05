8152cd509cc1bca78ee300468c3b134ac212a330
Noor-expertizo <noorulain@expertizo.pk>
2024-01-05 17:05:58 +0500

    checking

--- src/helpers/withAuth.tsx
+++ src/helpers/withAuth.tsx
@@ -4,13 +4,15 @@ import { useAuthStore } from "@/store/store";
 
 const withAuth = (Component: React.ComponentType<any>) => {
   return function WithAuth(props: any) {
-    const isAuthenticated = useAuthStore((state:any) => state.isAuthenticated);
+    const isAuthenticated = useAuthStore((state: any) => state.isAuthenticated);
     const router = useRouter();
 
     useEffect(() => {
+      console.error("Before redirection");
       if (!isAuthenticated) {
         router.push("/login");
       }
+      console.error("After redirection");
     }, [isAuthenticated]);
 
     if (!isAuthenticated) {
